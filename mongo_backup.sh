a0d06c4b648f6bd54072a967cb9d6b1965ba15a7
Francisco Panis Kaseker <franciscopk.ilax@gmail.com>
2020-12-02 00:08:48 -0300

    first version

---
+++ mongo_backup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# how to call the script
+# /opt/mongodb-backup/mongo_backup.sh NumberOfDaysOfRetention NumberOfPigzThreads
+
+# install cronjob
+installCronjob()
+{
+if [[ ! -f /etc/cron.d/mongodb-backup ]]
+then
+  cat <<EOF> /etc/cron.d/mongodb-backup
+SHELL=/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 4 * * * /opt/mongodb-backup/mongo_backup.sh $NumberOfDaysOfRetention $NumberOfPigzThreads
+EOF
+fi
+}
+
+# get timestamp
+timestamp()
+{
+  date +"%s" # current time
+}
+
+# global vars
+backup_date=$(timestamp)
+backup_path=/backup
+backup_numberofdays=$1
+pigz_threads=$2
+
+# install cronjob
+installCronjob
+
+# guarantee that dir exist and enter there
+mkdir -p $backup_path
+cd $backup_path
+
+# dump and compress
+mongodump &> /dev/null
+tar --remove-files --use-compress-program="pigz -p${pigz_threads} --rsyncable" -cf mongodb.dump.$backup_date.tar.gz dump
+
+# find and remove backups older than 7 days
+find $backup_path -mtime $backup_numberofdays -exec rm -f {} \;
